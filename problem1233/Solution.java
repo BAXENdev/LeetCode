17150b354926359f895cf5c06caf563e68332c2e
BAXENdev <83290629+BAXENdev@users.noreply.github.com>
2025-07-21 02:27:50 -0400

    Solved daily problem 1233

--- problem1233/Solution.java
+++ problem1233/Solution.java
@@ -1,6 +1,77 @@
 
 package problem1233;
 
+import java.util.List;
+import java.util.Set;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Solution {
-    
+    public List<String> removeSubfolders(String[] folder) {
+        Set<String> folderSet = new HashSet<>(Arrays.asList(folder));
+        List<String> rootFolders = new ArrayList<>();
+        // List<String> rootFolders = new ArrayList<>(Arrays.asList(folder));
+        // Collections.sort(rootFolders);
+        for (int i = 0; i < folder.length; i++) {
+            String subFolder = folder[i];
+            String prefix = subFolder;
+            boolean isRootFolder = true;
+            while (!prefix.isEmpty()) {
+                int lastSlashPos = prefix.lastIndexOf((char) '/');
+                if (lastSlashPos == 0) break;
+                prefix = prefix.substring(0, lastSlashPos);
+                if (folderSet.contains(prefix)) {
+                    isRootFolder = false;
+                    break;
+                }
+            }
+            if (isRootFolder) {
+                rootFolders.add(subFolder);
+            }
+        }
+
+        return rootFolders;
+    }
+
+
+
+    // FIRST SOLUTION
+    public List<String> removeSubfoldersOld(String[] folder) {
+        Set<String> folderSet = new HashSet<>(Arrays.asList(folder));
+        List<String> rootFolders = new ArrayList<>(Arrays.asList(folder));
+        Collections.sort(rootFolders);
+        for (int i = 0; i < rootFolders.size(); i++) {
+            String subFolder = rootFolders.get(i);
+            String[] subFolderArr = subFolder.substring(1).split("/");
+            List<Integer> toRemoveSubfolders = new ArrayList<>();
+            for (int j = i + 1; j < rootFolders.size(); j++) {
+                String subSubFolder = rootFolders.get(j);
+                String[] subSubFolderArr = subSubFolder.substring(1).split("/");
+                if (arrayStartsWith(subSubFolderArr, subFolderArr)) {
+                    toRemoveSubfolders.add(j);
+                } else {
+                    break;
+                }
+            }
+            int indexBuffer = 0;
+            for (int removeIndex : toRemoveSubfolders) {
+                rootFolders.remove(removeIndex - indexBuffer++);
+            }
+        }
+
+        return rootFolders;
+    }
+
+    public static boolean arrayStartsWith(String[] arr, String[] prefix) {
+        if (prefix.length >= arr.length) return false;
+        for (int i = 0; i < prefix.length; i++) {
+            if (!prefix[i].equals(arr[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- problem1233/test/SolutionTest.java
+++ problem1233/test/SolutionTest.java
@@ -7,8 +7,26 @@ import static org.junit.Assert.*;
 import problem1233.Solution;
 
 public class SolutionTest {
+    Solution solution = new Solution();
+
     @Test
     public void test1() {
+        String[] folders = {"/a","/a/b","/c/d","/c/d/e","/c/f"};
+        String[] expectedOutput = {"/a","/c/d","/c/f"};
+        assertArrayEquals(solution.removeSubfolders(folders).toArray(), expectedOutput);
+    }
 
+    @Test
+    public void test2() {
+        String[] folders = {"/a","/a/b/c","/a/b/d"};
+        String[] expectedOutput = {"/a"};
+        assertArrayEquals(solution.removeSubfolders(folders).toArray(), expectedOutput);
+    }
+
+    @Test
+    public void test3() {
+        String[] folders = {"/a/b/c","/a/b/ca","/a/b/d"};
+        String[] expectedOutput = {"/a/b/c","/a/b/ca","/a/b/d"};
+        assertArrayEquals(solution.removeSubfolders(folders).toArray(), expectedOutput);
     }
 }
